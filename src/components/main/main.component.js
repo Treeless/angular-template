8d53da40c2b4a8d3a7eba2d7f55daaeb79f145ff
Matthew Rowlandson <matt_94101@live.ca>
2016-06-17 06:56:36 -0400

    Little bit of controller action

--- src/components/main/main.component.js
+++ src/components/main/main.component.js
@@ -6,5 +6,18 @@
 
   function MainController() {
     var main = this;
+
+    var DEFAULT_TEXT = "Hello World"
+    main.text = DEFAULT_TEXT;
+
+    main.successClicked = function() {
+      main.text = "<span class='text-success' id='success-text'>You are successful!</span>"
+    };
+    main.failClicked = function() {
+      main.text = "<span class='text-danger' id='fail-text'>You have failed :(</span>"
+    };
+    main.reset = function() {
+      main.text = DEFAULT_TEXT;
+    };
   };
 }());
